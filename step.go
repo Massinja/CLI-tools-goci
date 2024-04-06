6e2e266be58e0916325bb566c3a0aea38514072b
Elviie Abdulmennanova <el123abd@gmail.com>
2024-04-06 15:16:24 -0400

    Define type step

---
+++ step.go
@@ -0,0 +1,38 @@
+package main
+
+import "os/exec"
+
+// A pipeline step.
+type step struct {
+	name    string   // step name
+	exe     string   // executable name of the external tool we want to execute
+	args    []string // arguments for the executable
+	message string   // output message in case of success
+	proj    string   // target project on which to execute the task
+}
+
+func (s step) execute() (string, error) {
+	cmd := exec.Command(s.exe, s.args...)
+	cmd.Dir = s.proj
+
+	if err := cmd.Run(); err != nil {
+		return "", &stepError{
+			step:  s.name,
+			msg:   "failed to execute",
+			cause: err,
+		}
+	}
+
+	return s.message, nil
+}
+
+// newStep instantiates and returns new step.
+func newStep(name, exe, message, proj string, args []string) step {
+	return step{
+		name:    name,
+		exe:     exe,
+		args:    args,
+		message: message,
+		proj:    proj,
+	}
+}
